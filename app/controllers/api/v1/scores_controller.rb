fb0ed8b90bf75b03cf5e5c2738a5a2a4ec6a1215
Rob McLarty <rob.mclarty@gmail.com>
2014-02-26 17:49:35 -0500

    update header settings in api controller

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -11,18 +11,18 @@ module Api
       def cors_set_access_control_headers
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
         headers['Access-Control-Max-Age'] = "1728000"
       end
 
-      # If this is a preflight OPTIONS request, then short-circuit the
-      # request, return only the necessary headers and return an empty
-      # text/plain.
-
       def cors_preflight_check
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-        headers['Access-Control-Max-Age'] = '1728000'
+        if request.method == "OPTIONS"
+          headers['Access-Control-Allow-Origin'] = 'http://localhost'
+          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+          headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+          headers['Access-Control-Max-Age'] = '1728000'
+          render :text => '', :content_type => 'text/plain'
+        end
       end
 
       def index
